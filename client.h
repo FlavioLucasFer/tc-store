94b0d997efc8e313ba887d069e76b5151a10eddf
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-05-17 18:42:10 -0300

    Client model

---
+++ client.h
@@ -0,0 +1,28 @@
+#ifndef __TC_STORE_CLIENT__
+#define __TC_STORE_CLIENT__
+
+#include "lib.h"
+
+uint16_t generate_client_id ();
+
+class client_t
+{
+	OO_ENCAPSULATE_RO(uint16_t, id);
+	OO_ENCAPSULATE(std::string, name);
+	OO_ENCAPSULATE(std::string, email);
+	OO_ENCAPSULATE(std::string, phone_number);
+
+	public:
+		client_t (std::string name, std::string email, std::string phone_number)
+		{
+			this->id = generate_client_id();	
+			this->name = name;
+			this->email = email;
+			this->phone_number = phone_number;
+		}
+		
+		std::string to_string ();
+};
+
+#endif
+
