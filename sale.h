393d770c8802b330f4d63740da155d85d247c8f0
Henrique Gonçalves de Oliveira <48062168+HenriqueGon@users.noreply.github.com>
2022-05-22 23:03:46 -0300

    Sale model

---
+++ sale.h
@@ -0,0 +1,35 @@
+#ifndef __TC_STORE_SALE__
+#define __TC_STORE_SALE__
+
+#include <string>
+
+#include "lib.h"
+#include "product.h"
+#include "client.h"
+
+uint16_t generate_sale_id ();
+
+class sale_t
+{
+	OO_ENCAPSULATE_RO(uint16_t, id);
+	OO_ENCAPSULATE(client_t, client);
+	OO_ENCAPSULATE(product_t, product);
+	OO_ENCAPSULATE(int, quantity);
+	OO_ENCAPSULATE(double, price);
+	
+	public:
+		sale_t () {}
+		
+		sale_t (client_t client, product_t product, int quantity, double price)
+		{	
+			this->id = generate_sale_id();
+			this->client = client;
+			this->product = product;
+			this->quantity = quantity;
+			this->price = price;
+		}
+	
+		std::string to_string();
+};
+
+#endif
