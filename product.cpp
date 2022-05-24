6882fe736d350c7d1bd4265ad49fd541a0f857db
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-05-24 17:31:33 -0300

    Product model

--- product.cpp
+++ product.cpp
@@ -1,13 +1,13 @@
 
 #include "product.h"
 
-uint16_t generate_product_id ()
+std::string product_t::fprice ()
 {
-	static uint16_t id = 1;
-	return id++;
+	std::string fprice = std::to_string(this->price);
+	return "$"+fprice;
 }
 
 std::string product_t::to_string ()
 {
-	return std::to_string(this->id) + " - " + this->name;
+	return std::to_string(this->id)+" - "+this->name+" (Price: "+this->fprice()+". amount: "+std::to_string(this->amount)+")";
 }
